ad741f1fd237b71c146644b6cf6fd6369eb5dca5
Prenume Nume <stefan-albert.alexandru@student.tuiasi.ro>
2022-07-31 20:09:08 +0300

    RegisterUser

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -1,21 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, from, Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { User } from '../Models';
+import { AddUser } from '../Models/userProfile';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  baseUrl = 'https://localhost:44365/Api/auth';
-  url ="auth/login";
 
+  baseUrl = 'https://localhost:44365/Api/User';
+  url ="auth/login";
+  register="User";
   constructor(private http : HttpClient) { }
 
 
   getUsers(credentials: User): Observable<any>{
     return this.http.post<User>(`${environment.apiURL}/${this.url}`, credentials);
+
+  }
+
+  addUsers(credentials: AddUser): Observable<any>{
+    return this.http.post<AddUser>(this.baseUrl, credentials)
 
   }
 
--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Route, Router } from '@angular/router';
+import { AddUser } from 'src/app/Models/userProfile';
+import { UserService } from 'src/app/Services/user.service';
 
 @Component({
   selector: 'app-register-page',
@@ -6,10 +11,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-page.component.scss']
 })
 export class RegisterPageComponent implements OnInit {
-
-  constructor() { }
+  FormGroupRegister!: FormGroup;
+  constructor(private addUserService: UserService, private router: Router, private http: HttpClient) { }
 
   ngOnInit(): void {
+
+    this.FormGroupRegister= new FormGroup(
+      {
+        username: new FormControl(''),
+        firstName: new FormControl(''),
+        lastName: new FormControl(''),
+        email: new FormControl(''),
+        phoneNumber: new FormControl(''),
+        hashed_password: new FormControl(''),
+      }
+    );
+
+    this.FormGroupRegister.valueChanges.subscribe(data=>{
+      console.log('values: ', data);
+    })
   }
 
+  onSubmitFormRegister(user: AddUser):void
+  {
+
+    console.log(user);
+    this.http.post("https://localhost:44365/api/user", user)
+    .subscribe({
+     next: res=>{
+      console.log(res);
+      this.router.navigate(["/login"]);
+
+     }
+
+    });
+
+    /*this.addUserService.addUsers({
+      Username: this.FormGroupRegister.get('username')?.value,
+      First_Name: this.FormGroupRegister.get('firstName')?.value,
+      Last_Name: this.FormGroupRegister.get('lastName')?.value,
+      Email: this.FormGroupRegister.get('email')?.value,
+      Phone_Number: this.FormGroupRegister.get('PhoneNumber')?.value,
+      Hashed_password: this.FormGroupRegister.get('password')?.value,
+      isAdmin: false
+
+    }).subscribe({
+      next: res=>{
+        this.router.navigate(["/login"]);
+      }
+    })
+  }*/
+  }
+
+
 }
+
